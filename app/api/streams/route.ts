bc3864058d8e64712aae6b7e44837edc4a6fc5fa
saahil <saahil992000@gmail.com>
2025-07-18 12:43:17 +0530

    trying to add default image

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -50,9 +50,8 @@ export async function POST(req: NextRequest) {
         let smallImg = "";
         let bigImg = "";
         if(dt.thumbnail === undefined || dt.thumbnail.thumbnails.length < 2) {
-            smallImg = "https://drive.google.com/file/d/1moFlzTi48bwpiOmVIW8vPkrjCowQYH1r/view?usp=sharing";
-            bigImg = "https://drive.google.com/file/d/1moFlzTi48bwpiOmVIW8vPkrjCowQYH1r/view?usp=sharing";
-            console.error("Thumbnail not found, using default image");
+            smallImg = "https://media.istockphoto.com/id/1175435360/vector/music-note-icon-vector-illustration.jpg?s=612x612&w=0&k=20&c=R7s6RR849L57bv_c7jMIFRW4H87-FjLB8sqZ08mN0OU=";
+            bigImg = "https://media.istockphoto.com/id/1175435360/vector/music-note-icon-vector-illustration.jpg?s=612x612&w=0&k=20&c=R7s6RR849L57bv_c7jMIFRW4H87-FjLB8sqZ08mN0OU=";
         }
         else{
             const length = dt.thumbnail.thumbnails.length;
--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
 
 module.exports = {
   images:{
-    domains: ['i.ytimg.com', "drive.google.com"],
+    domains: ['i.ytimg.com', "drive.google.com",'media.istockphoto.com', 'lh3.googleusercontent.com', 'yt3.googleusercontent.com'],
   }
 };
 
